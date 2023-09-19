55b48e7daa80bda4a26196b31598991f1c540bf9
Enzo <enzo.nguyen@avepoint.com>
2023-09-19 16:25:34 +0700

    EventSystem

---
+++ EventSystem/EventSystem/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+delegate void MessageHandler(string message);
+
+class NotificationCenter
+{
+    public event MessageHandler MessageReceived;
+
+    public void SendMessage(string message)
+    {
+        Console.WriteLine("Sending message: " + message);
+
+        if (MessageReceived != null)
+        {
+            MessageReceived(message);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        NotificationCenter notifier = new NotificationCenter();
+
+        notifier.MessageReceived += DisplayMessage;
+
+        notifier.SendMessage("Hello, My name is Enzo!");
+
+        notifier.MessageReceived -= DisplayMessage;
+
+        notifier.SendMessage("No one is listening!");
+
+        Console.ReadLine();
+    }
+    static void DisplayMessage(string message)
+    {
+        Console.WriteLine("Received message: " + message);
+    }
+}
+
